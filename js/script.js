3c669bff527e0d1956291fbea144a0fd919feeb5
eddiesaurus <128430976+eddiesaurus@users.noreply.github.com>
2023-04-05 14:46:49 -0400

    Code changes. Almost done!

--- js/script.js
+++ js/script.js
@@ -4,9 +4,14 @@ function showDetails() {
 	var x = document.getElementById("card");
 	var y = document.getElementById("details");
 	
+	var a = document.getElementById("cardBefore");
+	var b = document.getElementById("cardAfter");
+	
 	if (z !== true) {
 		x.style.backgroundPosition = "100% 0"; // Change the sprite for the card div.
 		y.style.display = "block"; // Show the details div.
+		a.style.display = "none"; // Hide the cardBefore div.
+		b.style.display = "block"; // Show the cardAfter div.
 		z = true;	
 	} else {
 		y.scrollIntoView({
